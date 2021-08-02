3bc6291ad3ee69a2205b01f9566dfa1972dd02f0
Areus-L <87375411+Areus-L@users.noreply.github.com>
2021-08-02 15:07:50 +0200

    components with routes has been created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './page/home/home.component';
+import { AddServiceComponent } from './page/add-service/add-service.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "add-service", component: AddServiceComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './page/home/home.component';
+import { AddServiceComponent } from './page/add-service/add-service.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AddServiceComponent
   ],
   imports: [
     BrowserModule,
