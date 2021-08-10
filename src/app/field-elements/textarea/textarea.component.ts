f2b6b314ec9d5d6eb3628960a07bb26be545800a
Areus-L <laszlo.romeo.csomali@areus.hu>
2021-08-10 09:44:19 +0200

    css modified

--- src/app/field-elements/textarea/textarea.component.ts
+++ src/app/field-elements/textarea/textarea.component.ts
@@ -7,16 +7,8 @@ import '@vaadin/vaadin-text-field/vaadin-text-area.js';
   selector: 'app-textarea',
   templateUrl: './textarea.component.html',
   styleUrls: ['./textarea.component.scss'],
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => TextareaComponent),
-      multi: true
-    }
-  ]
 })
-export class TextareaComponent implements OnInit, ControlValueAccessor {
-  //value = '';
+export class TextareaComponent implements OnInit {
   textLength: number = 0;
   @Input() maxLength: number;
   @Input() height: string;
@@ -28,37 +20,8 @@ export class TextareaComponent implements OnInit, ControlValueAccessor {
   }
 
   onChangeText(arg: any) {
-    console.log(arg);
     this.textLength = arg.target.value.length;
-    console.log(this.textLength);
   }
 
-  onChange: any = () => {}
-  onTouch: any = () => {}
-  val= ""
 
-  getKeys(O:any){
-    return Object.keys(O);
-  }
-
-  set value(val){
-    if( val !== undefined && this.val !== val){
-    this.val = val
-    this.onChange(val)
-    this.onTouch(val)
-    }
-
-  }
-
-  writeValue(value: any){
-    this.value = value
-  }
-
-  registerOnChange(fn: any){
-    this.onChange = fn
-  }
-
-  registerOnTouched(fn: any){
-    this.onTouch = fn
-  }
 }
